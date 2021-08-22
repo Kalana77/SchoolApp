e69e19a719563ba057cb4f8cd4e2b3e3f029274d
kalana <kalanasamararathna77@gmail.com>
2021-08-22 20:40:56 +0530

    adeded method to create student

--- backend/src/main/java/repository/StudentDao.java
+++ backend/src/main/java/repository/StudentDao.java
@@ -1,7 +1,31 @@
 package repository;
 
+import domain.Student;
+import utill.DbConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class StudentDao {
 
+    public void createStudent(Student stu) {
 
+        try {
+            Connection connection = DbConnector.getConnection();
+            String query = "insert into student(fname,lname,email,index_no,address,mobil_no) values (?,?,?,?,?,?)";
+            PreparedStatement prestm = connection.prepareStatement(query);
+            prestm.setString(1,stu.getFname());
+            prestm.setString(2,stu.getLname());
+            prestm.setString(3,stu.getEmail());
+            prestm.setString(4,stu.getIndexNo());
+            prestm.setString(5,stu.getAddress());
+            prestm.setString(6,stu.getMobileNum());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+    }
 
 }
