3b5402f86291304e10ddf0002616a0bd5b1ec6c7
kalana <kalanasamararathna77@gmail.com>
2021-08-22 20:40:19 +0530

    adeded DbConnector

---
+++ backend/src/main/java/utill/DbConnector.java
@@ -0,0 +1,26 @@
+package utill;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnector {
+
+    private static Connection con;
+
+    private DbConnector() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/school_app","root","");
+
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("class not found");
+        }
+    }
+
+    public static Connection getConnection() {
+        return con;
+    }
+    
+}
