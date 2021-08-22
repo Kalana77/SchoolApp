6a26cc665ffd35b13f147a9093f8bcfea32646bf
kalana <kalanasamararathna77@gmail.com>
2021-08-22 19:44:41 +0530

    create basic file structure

---
+++ backend/src/dto/StudentDto.java
@@ -0,0 +1,60 @@
+package dto;
+
+public class StudentDto {
+    private String fname;
+    private String lname;
+    private String email;
+    private String indexNo;
+    private String address;
+    private String mobileNum;
+
+
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIndexNo() {
+        return indexNo;
+    }
+
+    public void setIndexNo(String indexNo) {
+        this.indexNo = indexNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobileNum() {
+        return mobileNum;
+    }
+
+    public void setMobileNum(String mobileNum) {
+        this.mobileNum = mobileNum;
+    }
+}
